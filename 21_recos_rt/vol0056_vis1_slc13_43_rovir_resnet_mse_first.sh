4a23725943c3304c57fa8947094e5f804d2a7ea1
Blumenthal, Moritz <blumenthal@tugraz.at>
2024-08-14 16:35:20 +0200

    fixup weight path

--- 21_recos_rt/vol0056_vis1_slc13_43_rovir_resnet_mse_first.sh
+++ 21_recos_rt/vol0056_vis1_slc13_43_rovir_resnet_mse_first.sh
@@ -10,4 +10,4 @@ cd $SCRIPT_DIR/..
 RAWDIR=${RAWDIR:-$SCRIPT_DIR/../00_data/10_raw_data}
 RAW=$RAWDIR/rt_sa/vol0056_vis1
 
-01_scripts/41_eval_rt.sh -s13 -N $RAW 11_networks_rt/11_networks_rt/43_rovir_resnet_mse_first
+01_scripts/41_eval_rt.sh -s13 -N $RAW 11_networks_rt/43_rovir_resnet_mse_first
--- 21_recos_rt/vol0056_vis1_slc13_44_rovir_resnet_mad_first.sh
+++ 21_recos_rt/vol0056_vis1_slc13_44_rovir_resnet_mad_first.sh
@@ -10,4 +10,4 @@ cd $SCRIPT_DIR/..
 RAWDIR=${RAWDIR:-$SCRIPT_DIR/../00_data/10_raw_data}
 RAW=$RAWDIR/rt_sa/vol0056_vis1
 
-01_scripts/41_eval_rt.sh -s13 -N $RAW 11_networks_rt/44_rovir_resnet_mag_first
+01_scripts/41_eval_rt.sh -s13 -N $RAW 11_networks_rt/44_rovir_resnet_mad_first
