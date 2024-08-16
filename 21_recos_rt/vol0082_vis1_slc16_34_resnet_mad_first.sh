35caa269b416ac924270c9eab1ef3fb474135299
Moritz Blumenthal <84031251+mblum94@users.noreply.github.com>
2024-08-16 18:50:34 +0200

    fixup better split

--- 21_recos_rt/vol0082_vis1_slc16_34_resnet_mad_first.sh
+++ 21_recos_rt/vol0082_vis1_slc16_34_resnet_mad_first.sh
@@ -10,4 +10,4 @@ cd $SCRIPT_DIR/..
 RAWDIR=${RAWDIR:-$SCRIPT_DIR/../00_data/10_raw_data}
 RAW=$RAWDIR/rt_sa/vol0082_vis1
 
-01_scripts/41_eval_rt.sh -s16 -N $RAW 11_networks_rt/31_resnet_mse
+01_scripts/41_eval_rt.sh -s16 -N $RAW 11_networks_rt/34_resnet_mad_first
